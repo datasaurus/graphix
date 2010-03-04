dc6ea85a62b68710b1588fe9ef339ac445c19c8b
Gordon Carrie <dev0@trekix.net>
2010-03-04 16:01:15 +0000

    Made hsv application output less verbose

--- src/hsv.tcl
+++ src/hsv.tcl
@@ -4,26 +4,20 @@ exec wish "$0" "$@"
 
 # This application displays a sequence of labels with colors specified in stdin,
 # which should be output from hsv.
-# Each input line must have format:
-# 	"hue saturation value => color"
-# where color is acceptable to label's -background option
-# Each label will have text specifying "hue satuaration value"
+# Each input line must contain a "red green blue" specification for one color,
+# where the red, green, and blue values are floats in range [0,1].
 
 set i 0
 while {[gets stdin line] >= 0} {
-    set h [lindex $line 0]
-    set s [lindex $line 1]
-    set v [lindex $line 2]
-    set txt [format {%5.1f %5.1f %5.1f} $h $s $v]
-    set r [lindex $line 4]
-    set g [lindex $line 5]
-    set b [lindex $line 6]
+    set r [lindex $line 0]
+    set g [lindex $line 1]
+    set b [lindex $line 2]
     set c [format {#%02x%02x%02x}  \
 	    [expr {int($r * 255)}] \
 	    [expr {int($g * 255)}] \
 	    [expr {int($b * 255)}]]
     set l .l$i 
-    label $l -text $txt -background $c
+    label $l -width 6 -background $c
     pack $l -fill both -expand true
     incr i
 }
--- src/hsv_app.c
+++ src/hsv_app.c
@@ -7,7 +7,7 @@
  .
  .	Please send feedback to dev0@trekix.net
  .
- .	$Revision: 1.1 $ $Date: 2009/12/18 20:39:15 $
+ .	$Revision: 1.2 $ $Date: 2009/12/18 21:57:47 $
  */
 
 #include <stdlib.h>
@@ -89,7 +89,7 @@ int main(int argc, char *argv[])
     dh = (h1 - h0) / (n - 1);
     for (h = h0; n > 0; n--, h += dh) {
 	HSVtoRGB(&r, &g, &b, h, s, v);
-	printf("%lf %lf %lf => %lf %lf %lf\n", fmod(h, 360.0), s, v, r, g, b);
+	printf("%lf %lf %lf\n", r, g, b);
     }
     return 0;
 }
