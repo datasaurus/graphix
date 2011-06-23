f1b900ed402960a187bebbda048847742ce35013
Gordon Carrie <dev0@trekix.net>
2011-06-23 22:31:31 +0000

    Printout includes h, s, v, in addition to #rrggbb.

--- src/hsv_app.c
+++ src/hsv_app.c
@@ -7,7 +7,7 @@
  .
  .	Please send feedback to dev0@trekix.net
  .
- .	$Revision: 1.3 $ $Date: 2010/03/04 16:01:15 $
+ .	$Revision: 1.4 $ $Date: 2010/03/04 16:07:01 $
  */
 
 #include <stdlib.h>
@@ -89,7 +89,8 @@ int main(int argc, char *argv[])
     dh = (h1 - h0) / (n - 1);
     for (h = h0; n > 0; n--, h += dh) {
 	HSVtoRGB(&r, &g, &b, h, s, v);
-	printf("#%02x%02x%02x\n",
+	printf("%9.2f %9.2f %9.2f => #%02x%02x%02x\n",
+		h, s, v,
 		(unsigned)(r * 0xff), (unsigned)(g * 0xff), (unsigned)(b * 0xff));
     }
     return 0;
